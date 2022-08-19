bc346d15d1553a05f7dc2a20d27fdee00a74b005
Ashley Tucker <ashleyst35@aol.com>
2022-08-19 12:17:28 -0400

    bankingapp ex1

---
+++ BankingApp/BankAccount.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+
+
+        }
+        private double balance = 0; //hiding this balance from outside the class
+
+        // Define a method named Deposit that will accept a double
+        // and store that value in the balance field
+
+        // Encapsulation
+        public void Deposit(double amount)
+        {
+            balance = amount;
+  
+        }
+
+        //define a method named GetBalance that will return the amount stored in the balance  field
+
+        public string GetBalance()
+            //public double GetBalance() returns no $ sign in front 
+            //public string GetBalance() &  {return $"${balance}";} returns the number with money sign in front
+        {
+            return $"${balance}";
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    } 
+
+
+
+
+
+
+}
+
---
+++ BankingApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BankingApp;
+
+Console.WriteLine("Hello, World!");
+
+
+BankAccount account = new BankAccount();
+Console.WriteLine("How much would you like to deposit?");
+var amountToDep = double.Parse(Console.ReadLine()); 
+account.Deposit ( amountToDep);
+Console.WriteLine($"Thank you! Your balance is now {account.GetBalance()}");
